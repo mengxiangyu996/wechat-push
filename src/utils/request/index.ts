5fabb1a0b8abf8abc6df576eae3903cbf3bb8892
Zero <mengxiangyu996@qq.com>
2024-11-21 16:40:31 +0800

    chore: Update index.ts

--- src/utils/request/index.ts
+++ src/utils/request/index.ts
@@ -1,24 +1,34 @@
 import { createAxiosInstance } from "../../axios";
 import { AxiosConfig } from "../../axios/config";
 
-export const request = async <T>(url: string, method: string, data?: any, config?: AxiosConfig): Promise<T> => {
-    // 创建实例
-    let instance = createAxiosInstance(config);
+export interface RequestConfig {
+    url: string;
+    method: string;
+    headers?: Record<string, string>;
+    params?: any;
+    data?: any;
+    config?: AxiosConfig;
+}
+
+export default async <T>(config: RequestConfig): Promise<T> => {
+
+    const axiosInstance = createAxiosInstance(config.config);
 
     try {
-        let response = await instance.request({
-            url,
-            method,
-            data,
+        let response = await axiosInstance({
+            url: config.url,
+            method: config.method,
+            headers: config.headers,
+            params: config.params,
+            data: config.data,
         });
 
-        if (response.status !== 200) {
+        if (response.status != 200) {
             throw new Error(`Request failed with status ${response.status}, message: ${response.statusText}`);
         }
-        
+
         return response.data as T;
     } catch (error) {
         throw error;
     }
-
 }
